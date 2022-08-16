d09636a599e5488fe5221feddd92ca7886bedf07
MiIyama <naomi898@gmail.com>
2022-08-16 07:37:25 -0300

    Aula - Página Home

--- src/componentes/header/index.tsx
+++ src/componentes/header/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -12,6 +13,7 @@ export function Header() {
           </a>
           <a href="">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
